177a49dd8f4b8d53c10e8f7cc5f1e66ba04c9946
Michael Johnson <michael@johnson.computer>
2015-11-01 17:48:55 -0500

    Use more reliable method to get router address list contents

--- get-router-address-list.sh
+++ get-router-address-list.sh
@@ -9,4 +9,6 @@ die() {
 
 [ "$#" -eq 1 ] || die "Usage: $0 <SSH address of router>"
 
-ssh "$1" '/ip firewall address-list export' | grep -F 'list=spamhaus-drop' | grep -oE 'address=((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(/([1-2]?[0-9]|3[0-2]))?' | cut -d '=' -f2
+#ssh "$1" '/ip firewall address-list export compact' | grep -F 'list=spamhaus-drop' | grep -oE 'address=((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(/([1-2]?[0-9]|3[0-2]))?' | cut -d '=' -f2
+
+ssh "$1" ':foreach entry in=[/ip firewall address-list find list=spamhaus-drop] do={:put [/ip firewall address-list get $entry address]}'

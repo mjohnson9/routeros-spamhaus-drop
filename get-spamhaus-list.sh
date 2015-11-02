42e626b0d1ea6df4f2b0d8749cb0f83fe5c95949
Michael Johnson <michael@johnson.computer>
2015-11-02 11:50:18 -0500

    Add documentation

--- get-spamhaus-list.sh
+++ get-spamhaus-list.sh
@@ -4,6 +4,8 @@ set -e
 
 [ "$#" -eq 1 ] || die "Usage: $0 <drop list type (drop, edrop)>"
 
+# URL for drop list
 URL="https://www.spamhaus.org/drop/$1.lasso"
 
-curl -s -L -o - "${URL}" | sed 's/[[:blank:]]*;.*$//g' | grep -v '^$'
+# Download list, remove comments, and convert line endings to Unix
+curl -s -L -o - "${URL}" | sed 's/[[:blank:]]*;.*$//g' | grep -v '^$' | tr -d "\r"
--- update-router-drop-list.sh
+++ update-router-drop-list.sh
@@ -9,31 +9,39 @@ die() {
 
 [ "$#" -eq 1 ] || die "Usage: $0 <SSH address of router>"
 
+# Router's SSH address
 ROUTER="$1"
 
+# This script's directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Temporary directory for storing our working files
 TMPDIR="$(mktemp -d)"
 trap "rm -rf ${TMPDIR}" EXIT
 
+# Change into our temporary directory
 cd "${TMPDIR}"
 
+# Download the sanitized drop list from Spamhaus
 { "${SCRIPT_DIR}/get-spamhaus-list.sh" drop; "${SCRIPT_DIR}/get-spamhaus-list.sh" edrop; } | sort -u > spamhaus.txt
+# Download the list from the router
 "${SCRIPT_DIR}/get-router-address-list.sh" "${ROUTER}" | sort -u > router.txt
 
-dos2unix spamhaus.txt router.txt
-
+# Save the current time for the address list
 current_date="$(date --utc --rfc-3339=seconds)"
 
+# Find address that are in router.txt but not spamhaus.txt
 comm -23 router.txt spamhaus.txt | while read dl; do
 	echo "/ip firewall address-list remove [find list=spamhaus-drop address=${dl}]" >> script.txt
 done
 
+# Find addresses that are in spamhaus.txt but not router.txt
 comm -13 router.txt spamhaus.txt | while read dl; do
 	echo "/ip firewall address-list add list=spamhaus-drop comment=\"${current_date}\" address=${dl}" >> script.txt
 done
 
 if [ -e script.txt ]; then
+	# Connect to the router to apply changes
 	ssh "${ROUTER}" < script.txt
 	echo "Made $(wc -l script.txt) changes."
 else

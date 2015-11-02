95e0328d69391fdbd7b69d6a0ef5cfe662b67ba9
Michael Johnson <michael@johnson.computer>
2015-11-02 11:44:01 -0500

    Try new method for getting address list from router

--- get-router-address-list.sh
+++ get-router-address-list.sh
@@ -9,6 +9,17 @@ die() {
 
 [ "$#" -eq 1 ] || die "Usage: $0 <SSH address of router>"
 
-#ssh "$1" '/ip firewall address-list export compact' | grep -F 'list=spamhaus-drop' | grep -oE 'address=((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(/([1-2]?[0-9]|3[0-2]))?' | cut -d '=' -f2
+# Create printed address list on the router
+ssh "$1" "/ip firewall address-list print file=spamhaus-drop where list=spamhaus-drop"
 
-ssh "$1" ':foreach entry in=[/ip firewall address-list find list=spamhaus-drop] do={:put [/ip firewall address-list get $entry address]}'
+# Create a temporary file to store the list on our end
+TMPFILE="$(mktemp)"
+trap "rm -f ${TMPFILE}" EXIT
+
+# Download print list from the router
+scp "$1:/spamhaus-drop.txt" "${TMPFILE}"
+# Remove file in the background
+ssh "$1" "/file remove spamhaus-drop.txt" &
+
+# Extract the IP addresses from the print list and print it on stdout
+cat "${TMPFILE}" | grep -Fv ';;;' | grep -Fv '#' | grep -oE '(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))'
